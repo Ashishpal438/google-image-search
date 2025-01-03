cefe91e044f3260b2c651b22a341ab2675f45010
Ashish Pal <ashishpal438@gmail.com>
2025-01-03 15:59:36 +0530

    added linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['import'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+      },
+    ],
+  },
 };
